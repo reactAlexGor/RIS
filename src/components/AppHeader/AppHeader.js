ff96cd1bc6b69653d7ee9e7db7bab9c1312a9e3a
Alexey Goryainov <vladimirovich.alex.gor@gmail.com>
2024-07-08 08:43:55 +0200

    AppHeader

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,18 +1,30 @@
-import s from './AppHeader.module.scss'
-
-// import './AppHeader.scss'
+import './AppHeader.scss'
 
 const AppHeader = () => {
     return (
-        <header className={s.header}>
-            <nav className={s.nav}>
-                <input
-                    placeholder='Search...'
-                />
-            </nav>
-            <div>
+        <header className='header'>
+            <nav className='header__nav'>
+                <input placeholder='Search...' />
+                <div className="header__select">
+                    <p>API</p>
+                    <select >
+                        <option value='location'>location</option>
+                        <option value='character'>character</option>
+                    </select>
+                </div>
+                <h1>Choose a book format</h1>
+                <div class="select">
+                    <select name="format" id="format">
+                        <option selected disabled>Choose a book format</option>
+                        <option value="pdf">PDF</option>
+                        <option value="txt">txt</option>
+                        <option value="epub">ePub</option>
+                        <option value="fb2">fb2</option>
+                        <option value="mobi">mobi</option>
+                    </select>
+                </div>
 
-            </div>
+            </nav>
         </header >
     )
 }
